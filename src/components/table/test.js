dd3b74f5043ee40cf65e5c3be6c7a138d6042ea4
Rafael Pacheco <ravpacheco@gmail.com>
2019-11-13 15:27:06 -0300

    Add all documents

---
+++ src/components/table/test.js
@@ -0,0 +1,192 @@
+"businessChat": {
+    "name": "Business Chat",
+    "svg": "businessChat",
+    "support": {
+      "chatState": {
+        "isSupported": false
+      },
+      "image": {
+        "isSupported": true,
+        "modal": {
+          "title": "Test 1",
+          "description": "Desc 1",
+          "img": {
+            "src": "test",
+            "title": "test title",
+            "alt": "test alt"
+          },
+          "docsUrl": "https://docs.blip.ai/#multimedia-menu"
+        }
+      },
+      "menu": {
+        "isSupported": true,
+        "modal": {
+          "title": "Test 1",
+          "description": "Desc 1",
+          "img": {
+            "src": "test",
+            "title": "test title",
+            "alt": "test alt"
+          },
+          "docsUrl": "https://docs.blip.ai/#multimedia-menu"
+        }
+      },
+      "quickReply": {
+        "isSupported": false,
+        "modal": {
+          "title": "Test 1",
+          "description": "Desc 1",
+          "img": {
+            "src": "test",
+            "title": "test title",
+            "alt": "test alt"
+          },
+          "docsUrl": "https://docs.blip.ai/#multimedia-menu"
+        }
+      }
+    }
+  },
+  "whatsApp": {
+    "name": "WhatsApp",
+    "svg": "whatsApp",
+    "support": {
+      "chatState": {
+        "isSupported": false
+      },
+      "image": {
+        "isSupported": true,
+        "modal": {
+          "title": "Test 1",
+          "description": "Desc 1",
+          "img": {
+            "src": "test",
+            "title": "test title",
+            "alt": "test alt"
+          },
+          "docsUrl": "https://docs.blip.ai/#multimedia-menu"
+        }
+      },
+      "menu": {
+        "isSupported": false,
+        "modal": {
+          "title": "Test 1",
+          "description": "Desc 1",
+          "img": {
+            "src": "test",
+            "title": "test title",
+            "alt": "test alt"
+          },
+          "docsUrl": "https://docs.blip.ai/#multimedia-menu"
+        }
+      },
+      "quickReply": {
+        "isSupported": true,
+        "modal": {
+          "title": "Test 1",
+          "description": "Desc 1",
+          "img": {
+            "src": "test",
+            "title": "test title",
+            "alt": "test alt"
+          },
+          "docsUrl": "https://docs.blip.ai/#multimedia-menu"
+        }
+      }
+    }
+  },
+  "test": {
+    "name": "Test",
+    "svg": "messenger",
+    "support": {
+      "chatState": {
+        "isSupported": false
+      },
+      "image": {
+        "isSupported": true,
+        "modal": {
+          "title": "Test 1",
+          "description": "Desc 1",
+          "img": {
+            "src": "test",
+            "title": "test title",
+            "alt": "test alt"
+          },
+          "docsUrl": "https://docs.blip.ai/#multimedia-menu"
+        }
+      },
+      "menu": {
+        "isSupported": false,
+        "modal": {
+          "title": "Test 1",
+          "description": "Desc 1",
+          "img": {
+            "src": "test",
+            "title": "test title",
+            "alt": "test alt"
+          },
+          "docsUrl": "https://docs.blip.ai/#multimedia-menu"
+        }
+      },
+      "quickReply": {
+        "isSupported": true,
+        "modal": {
+          "title": "Test 1",
+          "description": "Desc 1",
+          "img": {
+            "src": "test",
+            "title": "test title",
+            "alt": "test alt"
+          },
+          "docsUrl": "https://docs.blip.ai/#multimedia-menu"
+        }
+      }
+    }
+  },
+  "test2": {
+    "name": "Test2",
+    "svg": "messenger",
+    "support": {
+      "chatState": {
+        "isSupported": false
+      },
+      "image": {
+        "isSupported": true,
+        "modal": {
+          "title": "Test 1",
+          "description": "Desc 1",
+          "img": {
+            "src": "test",
+            "title": "test title",
+            "alt": "test alt"
+          },
+          "docsUrl": "https://docs.blip.ai/#multimedia-menu"
+        }
+      },
+      "menu": {
+        "isSupported": false,
+        "modal": {
+          "title": "Test 1",
+          "description": "Desc 1",
+          "img": {
+            "src": "test",
+            "title": "test title",
+            "alt": "test alt"
+          },
+          "docsUrl": "https://docs.blip.ai/#multimedia-menu"
+        }
+      },
+      "quickReply": {
+        "isSupported": true,
+        "modal": {
+          "title": "Test 1",
+          "description": "Desc 1",
+          "img": {
+            "src": "test",
+            "title": "test title",
+            "alt": "test alt"
+          },
+          "docsUrl": "https://docs.blip.ai/#multimedia-menu"
+        }
+      }
+    }
+  }
