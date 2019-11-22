e848aae3b2166951add4a41c495b965d53d45639
caioqazz <caioqazz@gmail.com>
2019-11-22 12:49:07 -0300

    Permissions and view detail ABC, BC update

--- src/img/png/import.js
+++ src/img/png/import.js
@@ -1,5 +1,39 @@
 import test from "./test.png";
+import textBusinessChat from "./BusinessChat/text-business-chat.png";
+import mediaBusinessChat from "./BusinessChat/media-link-business-chat.png";
+import weblinkBusinessChat from "./BusinessChat/weblink-business-chat.png";
+import paymentInvoiceBusinessChat from "./BusinessChat/payment-invoice-business-chat.png";
+import audioBlipChat from "./BlipChat/audio-blip-chat.png";
+import carouselBlipChat from "./BlipChat/carousel-blip-chat.png";
+import chatStateBlipChat from "./BlipChat/chat-state-blip-chat.png";
+import documentBlipChat from "./BlipChat/document-blip-chat.png";
+import imageBlipChat from "./BlipChat/image-blip-chat.png";
+import menuBlipChat from "./BlipChat/menu-blip-chat.png";
+import quickReplyBlipChat from "./BlipChat/quick-reply-blip-chat.png";
+import requestLocationBlipChat from "./BlipChat/request-location-blip-chat.png";
+import sendLocationBlipChat from "./BlipChat/send-location-blip-chat.png";
+import textBlipChat from "./BlipChat/text-blip-chat.png";
+import videoBlipChat from "./BlipChat/video-blip-chat.png";
+import weblinkBlipChat from "./BlipChat/weblink-blip-chat.png";
+
+
 
 export const sampleImages = {
     test,
+    textBusinessChat,
+    mediaBusinessChat,
+    weblinkBusinessChat,
+    paymentInvoiceBusinessChat,
+    audioBlipChat,
+    carouselBlipChat,
+    chatStateBlipChat,
+    documentBlipChat,
+    imageBlipChat,
+    menuBlipChat,
+    quickReplyBlipChat,
+    requestLocationBlipChat,
+    sendLocationBlipChat,
+    textBlipChat,
+    videoBlipChat,
+    weblinkBlipChat
 };
--- src/img/svg/import.js
+++ src/img/svg/import.js
@@ -1,15 +1,33 @@
-import businessChat from "./businessChat.svg";
-import messenger from "./messenger.svg";
-import whatsApp from "./whatsApp.svg";
+import appleBusinessChat from "./business-chat-logo.svg";
+import blipChat from "./blipchat-logo.svg";
+import email from "./mailgun-logo.svg";
+import googleAssistant from "./google-assistant-logo.svg";
+import googleRcs from "./google-rcs-logo.svg";
+import infoBip from "./infobip-logo.svg";
+import messenger from "./messenger-logo.svg";
+import skype from "./skype-logo.svg";
+import telegram from "./telegram-logo.svg";
+import whatsApp from "./whatsapp.svg";
+import workplace from "./workplace-logo.svg";
+
 
 import image from "./image.svg";
 import menu from "./menu.svg";
 import quickReply from "./quickReply.svg";
 
 export const channelSvgs = {
-    businessChat,
+    appleBusinessChat,
+    blipChat,
+    email,
+    googleAssistant,
+    googleRcs,
+    infoBip,
     messenger,
+    skype,
+    telegram,
     whatsApp,
+    workplace
+    
 };
 
 export const contentTypeSvgs = {
